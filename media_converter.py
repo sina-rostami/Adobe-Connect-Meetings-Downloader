39c92dfb60620af9ba00bc361c7f9ac189147f22
sinarostami <Sinarostami4188.1@gmail.com>
2020-11-16 13:24:55 +0330

    add exception handling to media converter

--- media_converter.py
+++ media_converter.py
@@ -8,18 +8,23 @@ def convert_audio(file_name):
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     for file in os.listdir(directory_path):
-        if file.startswith('camera') and file.endswith('.flv'):
-            print('converting '+ directory_path + file)
-            audio = ffmpeg.input(directory_path + file).audio
-            ffmpeg.output(audio, output_directory + file.split('.')[0] + '.ogg', loglevel='quiet').run()
+        try:
+            if file.startswith('camera') and file.endswith('.flv'):
+                print('converting '+ directory_path + file)
+                audio = ffmpeg.input(directory_path + file).audio
+                ffmpeg.output(audio, output_directory + file.split('.')[0] + '.ogg', loglevel='quiet').run()
+        except:
+            pass
 def convert_video(file_name):
-    ffmpeg.loglevel = 'info'
     directory_path = './temp/'+file_name+'/'
     output_directory = './output/'+file_name+'/'
     if not os.path.exists(output_directory):
         os.makedirs(output_directory)
     for file in os.listdir(directory_path):
-        if file.startswith('screen') and file.endswith('.flv'):
-            print('converting '+ directory_path + file)
-            video = ffmpeg.input(directory_path + file).video
-            ffmpeg.output(video, output_directory + file.split('.')[0] + '.mkv', codec='copy', loglevel='quiet').run()
+        try:
+            if file.startswith('screen') and file.endswith('.flv'):
+                print('converting '+ directory_path + file)
+                video = ffmpeg.input(directory_path + file).video
+                ffmpeg.output(video, output_directory + file.split('.')[0] + '.mkv', codec='copy', loglevel='quiet').run()
+        except:
+            pass
