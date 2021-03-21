db884290ff8697868a3b97a61fbee046fb1ee9ae
Matin <itsmatinhimself@gmail.com>
2021-03-21 14:56:48 +0330

    Filename scrape feature

--- ikiu_vc_dl.py
+++ ikiu_vc_dl.py
@@ -1,3 +1,4 @@
+import requests
 from downloader import Downloader
 import time
 import re
@@ -28,27 +29,32 @@ class IkiuDownloader(Downloader):
 
     def create_downlaod_link(self):
         self.download_link = f'{self.base_download_url}/{self.file_id}/output/{self.file_id}.zip?download=zip'
-        print(self.download_link)
         return True
 
     def batch_download(self, links: list[str]):
+        self.set_cookies()
         for url in links:
             if re.match(r'https://ac\.aminidc\.com/(.*)/.*', url):
-                filename = re.findall('https://ac\.aminidc\.com/(.*)/.*', url)[0]
-                self.file_id = filename
-                print('Downloading ' + filename + '...')
-                self.set_name_to_save(filename)
+                self.file_id = re.findall('https://ac\.aminidc\.com/(.*)/.*', url)[0]
+                src = requests.get(url, headers=self.download_headers)
+                t = re.findall('roomNameForMobile = "(.*?)";', src.text)[0]
+                self.name_to_save = t.encode().decode("unicode-escape")
+                print('Downloading ' + self.name_to_save + '...')
                 self.set_pasted_url(url)
-                self.set_cookies()
                 self.create_downlaod_link()
+
                 self.download_file()
                 self.save_file()
                 self.extract_zip_file()
                 self.convert_media()
                 self.download_other_files()
-                print(filename + ' downloaded!')
+                print(self.name_to_save + ' downloaded!')
+            else:
+                print('invalid url ')
         self.remove_temp_directory()
 
+async def convertq(arg):
+    pass
 
 if __name__ == '__main__':
     with open('info.txt', 'r') as f:
