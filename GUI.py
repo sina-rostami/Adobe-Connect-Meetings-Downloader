e79ed534470bf201279d6274e30ae33fa5a397d7
Sina Rostami <61062593+sina-rostami@users.noreply.github.com>
2021-06-19 19:43:07 +0430

    Fix gui (#23)
    * Add University Of Tehran
    
    * Make IKIU consistent to other modules
    
    * Fix Ut name
    
    * Make Gui consistent

--- GUI.py
+++ GUI.py
@@ -1,5 +1,7 @@
 import PySimpleGUI as sg
 import kntu_vc_dl
+import ut_vc_dl
+import ikiu_vc_dl
 
 sg.theme("Topanga")
 
@@ -7,16 +9,22 @@ sg.theme("Topanga")
 def download_meeting(values, window):
     window.close()
     pasted_urls = [l for l in values['-LINKS-'].split("\n") if len(l) > 0]
-    kntu_vc_dl.kntu_download(
-        values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
+    if values['-UNI-'] == 'Khaje Nasir Toosi University Of Technology':
+        kntu_vc_dl.kntu_download(
+            values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
+    elif values['-UNI-'] == 'University Of Tehran':
+        ut_vc_dl.ut_download(values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
+    elif values['-UNI-'] == 'Imam Khomeini International University':
+        ikiu_vc_dl.ikiu_download(values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
 
 
 layout = [
     [sg.T('"Welcome To Adobe Connect Meetings Downloader"', pad=(220, 0))],
+    [sg.T('University:')], [sg.Combo(['Khaje Nasir Toosi University Of Technology', 'University Of Tehran', 'Imam Khomeini International University'], key='-UNI-')],
     [sg.T('UserName:')], [sg.Input(key='-USERNAME-', size=(25, 1))],
     [sg.T('Password:')], [sg.Input(key='-PASSWD-', size=(25, 1))],
     [sg.T('Mettings Links:')], [sg.MLine(key='-LINKS-', size=(150, 20))],
-    [sg.Button('Download', pad=(350,1))]
+    [sg.Button('Download', pad=(350,1))],
 ]
 
 window = sg.Window('Adobe Connect Meetings Downloader',
