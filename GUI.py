5a38b58beea987b09a34aec4e878bcc91eeb0733
Sina Rostami <61062593+sina-rostami@users.noreply.github.com>
2021-09-24 15:27:10 +0330

    Update GUI.py

--- GUI.py
+++ GUI.py
@@ -4,6 +4,7 @@ import PySimpleGUI as sg
 import export_only
 import kntu_vc_dl
 import ut_vc_dl
+import iut_vc_dl
 import ikiu_vc_dl
 
 sg.theme("Topanga")
@@ -17,20 +18,22 @@ def download_meeting(values, window):
             values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
     elif values['-UNI-'] == 'University Of Tehran':
         ut_vc_dl.ut_download(values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
+    elif values['-UNI-'] == 'Isfahan Uinversity Of Technology':
+        iut_vc_dl.iut_download(values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
     elif values['-UNI-'] == 'Imam Khomeini International University':
         ikiu_vc_dl.ikiu_download(values['-USERNAME-'], values['-PASSWD-'], pasted_urls)
 
 
 layout = [
-    [sg.T('"Welcome To Adobe Connect Meetings Downloader"', pad=(220, 0))],
-    [sg.T('University:')], [sg.Combo(['Khaje Nasir Toosi University Of Technology', 'University Of Tehran', 'Imam Khomeini International University'], key='-UNI-')],
-    [sg.T('UserName:')], [sg.Input(key='-USERNAME-', size=(25, 1))],
+    [sg.T('"Adobe Connect Meetings Tools"', pad=(220, 0))],
+    [sg.T('University:')], [sg.Combo(['Khaje Nasir Toosi University Of Technology', 'University Of Tehran', 'Isfahan Uinversity Of Technology','Imam Khomeini International University'], key='-UNI-')],
+    [sg.T('Username:')], [sg.Input(key='-USERNAME-', size=(25, 1))],
     [sg.T('Password:')], [sg.Input(key='-PASSWD-', size=(25, 1))],
     [sg.T('Mettings Links:')], [sg.MLine(key='-LINKS-', size=(150, 8))],
     [sg.Button('Download'), sg.Button('Export-Only')],
 ]
 
-window = sg.Window('Adobe Connect Meetings Downloader',
+window = sg.Window('Adobe Connect Meetings Tools',
                    layout, size=(800, 450), grab_anywhere=True, resizable=True)
 
 while True:
